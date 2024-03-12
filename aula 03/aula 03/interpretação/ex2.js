71d7e174a4562e6b0d67238b9e0ad8bfe516849c
VithorLeonardoMS <162487463+VithorLeonardoMS@users.noreply.github.com>
2024-03-12 17:10:53 -0300

    Add files via upload

---
+++ aula 03/aula 03/interpretação/ex2.js
@@ -0,0 +1,7 @@
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c)
+//O código imprime: 10 10 10
---
+++ aula 03/aula 03/outros/testesaleartorios.js
@@ -0,0 +1,12 @@
+let nome = 'Vithor Leonardo Mello Soares'
+let idade = 17
+let altura = 1.61
+let verdadeiroIdade
+let verdadeiroAltura
+if (idade<16) {verdadeiroIdade = true
+    
+} else {verdadeiroIdade = false
+    
+}
+if (altura<1.70) {verdadeiroAltura = false}
+else {verdadeiroAltura = true}
