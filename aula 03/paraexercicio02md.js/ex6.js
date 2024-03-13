997f82df7da8150104a515de86f463885aaa0407
VithorLeonardoMS <162487463+VithorLeonardoMS@users.noreply.github.com>
2024-03-13 14:38:36 -0300

    Add files via upload

---
+++ aula 03/paraexercicio02md.js/ex6.js
@@ -0,0 +1,6 @@
+let a = '25'
+let b = '50'
+let somaStrings = a + b
+console.log(somaStrings)//Aqui é impresso o texto 25 e 50, que juntos são 2550
+const somaNumbers = Number(a) + Number(b)//Aqui transformei as váriaveis em numbers antes de somalas
+console.log(somaNumbers)
