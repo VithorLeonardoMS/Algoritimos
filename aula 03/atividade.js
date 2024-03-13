c89bc992552498a6d810e6c93535db1e523d69e6
VithorLeonardoMS <162487463+VithorLeonardoMS@users.noreply.github.com>
2024-03-13 13:45:46 -0300

    Delete aula 03/atividade.js

--- aula 03/atividade.js
+++
@@ -1,5 +0,0 @@
-const meuNome = 'Vithor'
-const meuSobrenome = 'Leonardo'
-let minhaIdade = 17
-let souEstudante = true
-console.log('Meu nome é',meuNome,', meusobrenome é',meuSobrenome,'tenho',minhaIdade,'anos, e meu status de estudante é', souEstudante)
