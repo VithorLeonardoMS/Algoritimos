093284e9cb696c830581c5c613256d6ee79b66cd
VithorLeonardoMS <162487463+VithorLeonardoMS@users.noreply.github.com>
2024-03-25 17:26:41 -0300

    Add files via upload

---
+++ Testes/radiciação/radiciacaoesbocos.js
@@ -0,0 +1,20 @@
+let indice = 2//de cima
+let radicando = 4//de baixo
+let dezena
+let unidade
+let maior
+let menor
+let variavel
+function primeiroteste(a,b){
+
+maior = 100
+menor = 0
+while () {
+    
+
+    if(radicando>=menor && radicando <= maior ){
+        dezena = 0
+    }
+variavel = maior
+maior
+}}
