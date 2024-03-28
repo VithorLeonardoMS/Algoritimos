836343b4be4c50d2119fb6d1b7914dab7bdf6ec5
VithorLeonardoMS <162487463+VithorLeonardoMS@users.noreply.github.com>
2024-03-28 16:40:55 -0300

    Add files via upload

---
+++ aula.28.03.24/funcoesExeercicicos/Nivel01/ex03.js
@@ -0,0 +1,5 @@
+//Número de caracteres em uma string: Faça uma função que receba uma string como parâmetro e retorne o número de caracteres dessa string. Você deve usar return.
+function numCaracteres(string){
+    return string.length
+}
+console.log('O número de caractéres é:', numCaracteres("abc"))
---
+++ aula.28.03.24/funcoesExeercicicos/Nivel01/ex07.js
@@ -0,0 +1,5 @@
+//Imprimir frase: Escreva uma função que receba um nome, uma idade e sua série favorita e retorne a frase “Olá [NOME]! Sua idade é [IDADE] e você gosta de assistir [SERIE]!”. Você deve usar return.
+const imprecao = (nome, idade, serie) => {
+return `Olá ${nome}! Sua idade é ${idade} e você gosta de assistir ${serie}!`
+}
+console.log(imprecao('PetrusDuGrau', '15', 'Um pirce'))
