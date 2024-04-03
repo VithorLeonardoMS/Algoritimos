54c46683051c7955c10cc1461526781b731c8535
VithorLeonardoMS <162487463+VithorLeonardoMS@users.noreply.github.com>
2024-04-03 17:23:51 -0300

    Add files via upload

---
+++ 03.04.24/desafio/desafio.js
@@ -0,0 +1,11 @@
+/*
+1. Receba uma frase e retorne um array onde cada elemento é uma das palavras da frase, ignorando os espaços
+*/
+const readlineSync = require('readline-sync');
+
+const frase = readlineSync.question('Diga uma frase: ');
+
+//nao fiz
+
+
+
